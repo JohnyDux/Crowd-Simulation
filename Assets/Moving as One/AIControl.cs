920ec0bdced8c662f7fa5801b46dcea3b756069c
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-10-22 00:32:46 +0100

    Adding animation

--- Assets/Moving as One/AIControl.cs
+++ Assets/Moving as One/AIControl.cs
@@ -7,12 +7,15 @@ public class AIControl : MonoBehaviour
 {
     GameObject [] goalLocations;
     NavMeshAgent agent;
+    Animator anim;
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
         goalLocations = GameObject.FindGameObjectsWithTag("goal");
         int i = Random.Range(0, goalLocations.Length);
         agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
     }
 
     
